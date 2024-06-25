750ce0e7ade1db4473258ae0e40923921eb17b92
Okuda0921 <c0a2303274@edu.teu.ac.jp>
2024-06-25 17:37:38 +0900

    追加課題4:direction完成

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -3,6 +3,7 @@ import random
 import sys
 import time
 import pygame as pg
+import math
 
 
 WIDTH = 1100  # ゲームウィンドウの幅
@@ -56,6 +57,7 @@ class Bird:
         self.img = __class__.imgs[(+5, 0)]
         self.rct: pg.Rect = self.img.get_rect()
         self.rct.center = xy
+        self.dire = (+5, 0)
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -77,6 +79,8 @@ class Bird:
             if key_lst[k]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+        if sum_mv != [0, 0]:
+            self.dire = tuple(sum_mv)
         self.rct.move_ip(sum_mv)
         if check_bound(self.rct) != (True, True):
             self.rct.move_ip(-sum_mv[0], -sum_mv[1])
@@ -97,7 +101,13 @@ class Beam():
         self.rct = self.img.get_rect()
         self.rct.centery = bird.rct.centery
         self.rct.left = bird.rct.right
-        self.vx, self.vy = +5, 0
+        self.vx, self.vy = bird.dire[0], bird.dire[1]
+        self.sita = math.atan2(-self.vy, self.vx)
+        self.sita1 = math.degrees(self.sita)
+        self.img = pg.transform.rotozoom(self.img, self.sita1, 1)
+        print(self.sita1)
+        self.rct.centerx = bird.rct.centerx + (bird.rct.right - bird.rct.left) * self.vx / 5
+        self.rct.centery = bird.rct.centery + (bird.rct.bottom - bird.rct.top) * self.vy / 5
 
     def update(self, screen: pg.Surface):
         """
